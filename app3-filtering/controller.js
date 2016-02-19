8ada322df2edf640c9366b17f3440d5906d9f459
Josh Barber <JoshsMacbook@Joshs-MacBook-Pro.local>
2016-02-19 11:42:39 -0700

    finished try1 app3

--- app3-filtering/controller.js
+++ app3-filtering/controller.js
@@ -1,3 +1,8 @@
 angular.module('app3').controller('mainCtrl', function($scope, svc){
   $scope.data = svc.getData();
+
+  $scope.filterObj = {
+    email: ".uk",
+    phone:"1"
+  }
 })
--- app3-filtering/service.js
+++ app3-filtering/service.js
@@ -1,5 +1,9 @@
 var data = [
-	{"name": "Aileen Jacobs", "email": "convallis.ante.lectus@euneque.co.uk", "dateEmployed": "01/01/2016", "phone": "1-862-996-4921"},
+	{"name": "Aileen Jacobs",
+	"email": "convallis.ante.lectus@euneque.co.uk",
+	"dateEmployed": "01/01/2016",
+	"phone": "1-862-996-4921"
+	},
 	{"name": "Karen Delgado", "email": "dapibus@Morbiquis.ca", "dateEmployed": "10/03/2014", "phone": "403-8410"},
 	{"name": "Ursula Melendez", "email": "sit.amet@nullaInteger.edu", "dateEmployed": "07/06/2016", "phone": "1-818-102-1801"},
 	{"name": "Kalia Chan", "email": "commodo.ipsum.Suspendisse@Sed.net", "dateEmployed": "12/09/2014", "phone": "535-2986"},
